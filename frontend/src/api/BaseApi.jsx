a52a0129ffb3296c0466252420902bdde0054602
IrfanAnsari <96404366+iRfAn-AnSaRi0@users.noreply.github.com>
2025-07-15 23:11:26 -0300

    Update BaseApi.jsx

--- frontend/src/api/BaseApi.jsx
+++ frontend/src/api/BaseApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task-1-ngd8.onrender.com",
+  baseURL: "https://task-1-ngd8.onrender.com/api",
   withCredentials: true
 });
 
